4b0034aad96fd41489f018d09f01c7c4068a92ff
P. Arnon <32287948+chainorato@users.noreply.github.com>
2024-05-30 12:50:11 +0900

    Update 03_Intestinal_markers_wt_ACE2_and_TMPRSS2.R

--- colon_organoid_gene_expression_comparison/03_Intestinal_markers_wt_ACE2_and_TMPRSS2.R
+++ colon_organoid_gene_expression_comparison/03_Intestinal_markers_wt_ACE2_and_TMPRSS2.R
@@ -2,7 +2,7 @@ library(ComplexHeatmap) # Heatmap
 library(circlize)
 
 ##### Create a heatmap of log2FC of ACE2, TMPRSS2, and intestinal marker genes (Supplementary Figure 4A)
-colon_gene_df <- read.delim("~/Desktop/BANAL_project/240213_Colon_expression/240213_colon_gene_expression.txt", header=TRUE)
+colon_gene_df <- read.delim("~/240213_Colon_expression/240213_colon_gene_expression.txt", header=TRUE)
 colnames(colon_gene_df) <- c("Group","ACE2","CYP2C19","SLC51A","SLC51B","CA2","CDX2","TMPRSS2","VIL1","ABCC2")
 colon_gene_df$Group <- c("Colon organoid","Caco-2","Adult small intestine","Fetal small intestine")
 rownames(colon_gene_df) <- colon_gene_df$Group
